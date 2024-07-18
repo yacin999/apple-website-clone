ad7c75761e5a8a61dcbdd6f43fdd473ccd6760a9
yacine <yacinomar999@gmail.com>
2024-07-18 17:00:30 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import {
 const App = () => {
   return (
     <div className="bg-black min-h-screen w-full">
+      {/* these components are for learning perpses */}
       <Router>
         <Routes>
           <Route path="/gsapto" element={<GsapTo />} />
