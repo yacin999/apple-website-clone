738a036c7f902da5f480f4641f5edce2f64f63d1
yacine <yacinomar999@gmail.com>
2024-07-25 19:56:55 +0100

    add scope to usescroll

--- src/pages/GsapScrollTrigger.jsx
+++ src/pages/GsapScrollTrigger.jsx
@@ -16,20 +16,22 @@ const GsapScrollTrigger = () => {
 
     boxes.forEach(box => {
       gsap.to(box, {
-        x : 150,
+        x : 150 * (boxes.indexOf(box) + 5),
         rotation : 360,
         borderRadius : '100%',
         scale : 1.5,
         scrollTrigger : {
           trigger : box,
-          start : "bottom, bottom",
-          end : "top 20%",
+          start : "bottom bottom",
+          end : "top 30%",
           scrub : true
         },
         ease : "power1.inOut"
       })
     })
-  }, [])
+  }, {
+    scope : scrollRef
+  })
 
 
   return (
